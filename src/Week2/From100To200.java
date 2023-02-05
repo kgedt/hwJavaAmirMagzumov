4f89a9e71ee365e9d164c23b27925344776aeb6d
kgedt <magzumov7162@gmail.com>
2023-02-05 22:03:16 +0600

    A10's brrrrt google it

--- src/Week2/From100To200.java
+++ src/Week2/From100To200.java
@@ -2,11 +2,11 @@ package Week2;
 
 public class From100To200 {
     public static void main(String[] args) {
-        int counter = 0;
+        int counter = 1;
         for (int i = 100; i <= 200; ++i) {
             if ((i % 5 == 0 || i % 6 == 0) && (i % (5 * 6) != 0)) {
                 System.out.print(i + " ");
-                if (counter % 10 == 0 && counter != 0) {
+                if (counter % 10 == 0) {
                     System.out.println();
                     counter++;
                 } else {
